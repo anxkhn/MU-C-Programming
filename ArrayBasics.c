fb91f0cbaaaa0a8fe7a63bcec05a278daf84ca84
Anas Khan <83116240+anxkhn@users.noreply.github.com>
2022-06-18 15:25:32 +0530

    Update ArrayBasics.c

--- ArrayBasics.c
+++ ArrayBasics.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 
-int main()
+void main()
 {
-    int avg, i, sum = 0;
-    int marks[n]; /* array declaration */
+    int i, sum = 0;
+    float avg;
+    int marks[5];
     for (i = 0; i <= 4; i++)
     {
         printf("Enter marks ");
         scanf("%d", & marks[i]); /* store data in array */
     }
     for (i = 0; i <= 4; i++)
+    {
         sum = sum + marks[i]; /* read data from an array*/
+    }
     avg = sum / 5;
-
-    return 0;
+    printf("Avg: %f", avg);
 }
